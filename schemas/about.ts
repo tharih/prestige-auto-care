c2fef85a274b6f50285a3141716d907cdd8508a4
slacker1992 <suvinjavax@gmail.com>
2023-02-08 13:24:56 +0530

    updated

---
+++ schemas/about.ts
@@ -0,0 +1,43 @@
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+  name: "about",
+  title: "About",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+    }),
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "string",
+    }),
+    defineField({
+      name: "experienceYears",
+      title: "Experience Years",
+      type: "string",
+    }),
+
+    defineField({
+      name: "options",
+      title: "Options",
+      type: "array",
+      of: [
+        {
+          type: "string",
+        },
+      ],
+    }),
+    defineField({
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    }),
+  ],
+});
--- schemas/index.ts
+++ schemas/index.ts
@@ -4,5 +4,14 @@ import post from "./post";
 import author from "./author";
 import slider from "./slider";
 import product from "./product";
+import about from "./about";
 
-export const schemaTypes = [post, author, category, blockContent, slider,product];
+export const schemaTypes = [
+  post,
+  author,
+  category,
+  blockContent,
+  slider,
+  product,
+  about,
+];

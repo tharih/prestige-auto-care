7d633d6eadfaa427bdec80fc90775ce67974913b
Tharinda Dissanayake <tharindadissanayake456@gmail.com>
2023-02-08 13:09:48 +0530

    sanity

--- schemas/index.ts
+++ schemas/index.ts
@@ -3,5 +3,6 @@ import category from "./category";
 import post from "./post";
 import author from "./author";
 import slider from "./slider";
+import product from "./product";
 
-export const schemaTypes = [post, author, category, blockContent, slider];
+export const schemaTypes = [post, author, category, blockContent, slider,product];

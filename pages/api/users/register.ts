2b83c62c80955fba7cfac8bfdc6e819e8ae44c9e
Tharinda Dissanayake <tharindadissanayake456@gmail.com>
2023-03-17 00:03:28 +0530

    gg

--- pages/api/users/register.ts
+++ pages/api/users/register.ts
@@ -1,8 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { UserType } from "../../../utils/type";
-import bcrypt from "bcryptjs";
+// import bcrypt from "bcryptjs";
 import axios from "axios";
-import { signToken } from "../../../utils/signToken";
+// import { signToken } from "../../../utils/signToken";
 
 type Data = {
   message: string;
@@ -15,40 +15,40 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  if (req.method === "POST") {
-    const createMutations = [
-      {
-        create: {
-          _type: "user",
-          name: req.body.name,
-          email: req.body.email,
-          password: bcrypt.hashSync(req.body.password),
-          isAdmin: false,
-        },
-      },
-    ];
-    const apiEndPoint = `https://${projectId}.api.sanity.io/v2022-11-15/data/mutate/${dataset}?returnIds=true`;
-    const { data } = await axios.post(
-      apiEndPoint,
-      { mutations: createMutations },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${tokenWithWriteAccess}`,
-        },
-      }
-    );
-    const userID = data.results[0].id;
-    const user = {
-      _id: userID,
-      name: req.body.name,
-      email: req.body.email,
-      isAdmin: false,
-    };
+  // if (req.method === "POST") {
+  //   const createMutations = [
+  //     {
+  //       create: {
+  //         _type: "user",
+  //         name: req.body.name,
+  //         email: req.body.email,
+  //         password: bcrypt.hashSync(req.body.password),
+  //         isAdmin: false,
+  //       },
+  //     },
+  //   ];
+  //   const apiEndPoint = `https://${projectId}.api.sanity.io/v2022-11-15/data/mutate/${dataset}?returnIds=true`;
+  //   const { data } = await axios.post(
+  //     apiEndPoint,
+  //     { mutations: createMutations },
+  //     {
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         Authorization: `Bearer ${tokenWithWriteAccess}`,
+  //       },
+  //     }
+  //   );
+  //   const userID = data.results[0].id;
+  //   const user = {
+  //     _id: userID,
+  //     name: req.body.name,
+  //     email: req.body.email,
+  //     isAdmin: false,
+  //   };
 
-    const token = signToken(user);
+  //   const token = signToken(user);
 
-    // @ts-ignore
-    res.send({ ...user, token });
-  }
+  //   // @ts-ignore
+  //   res.send({ ...user, token });
+  // }
 }

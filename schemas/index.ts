38a8e09067fc1bdbf1521b19830b8efcfe059651
Tharinda Dissanayake <tharindadissanayake456@gmail.com>
2023-03-16 11:56:32 +0530

    update

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,38 +1,38 @@
 import blockContent from "./blockContent";
 import category from "./category";
-import post from "./post";
+// import post from "./post";
 import author from "./author";
-import slider from "./slider";
+// import slider from "./slider";
 import product from "./product";
 import about from "./about";
-import services from "./services";
-import user from "./user";
-import orderItem from "./orderItem";
-import orders from "./orders";
-import banners from "./banners";
-import workProcess from "./workProcess";
-import paintDetails from "./paintDetails";
-import mechanicalDetails from "./mechanicalDetails";
-import gallery from "./gallery";
-import blog from "./blog";
+// import services from "./services";
+// import user from "./user";
+// import orderItem from "./orderItem";
+// import orders from "./orders";
+// import banners from "./banners";
+// import workProcess from "./workProcess";
+// import paintDetails from "./paintDetails";
+// import mechanicalDetails from "./mechanicalDetails";
+// import gallery from "./gallery";
+// import blog from "./blog";
 
 
 export const schemaTypes = [
-  post,
+  // post,
   author,
   category,
   blockContent,
-  slider,
-  banners,
-  workProcess,
+  // slider,
+  // banners,
+  // workProcess,
   product,
-  blog,
+  // blog,
   about,
-  gallery,
-  services,
-  paintDetails,
-  mechanicalDetails,
-  user,
-  orders,
-  orderItem,
+  // gallery,
+  // services,
+  // paintDetails,
+  // mechanicalDetails,
+  // user,
+  // orders,
+  // orderItem,
 ];

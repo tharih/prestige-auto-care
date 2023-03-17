ded150898c641006098b8dd6c06cb373c7b1dc2e
Tharinda Dissanayake <tharindadissanayake456@gmail.com>
2023-03-17 07:46:53 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    domains: ["cdn.sanity.io"],
+  },
+
+  env: {
+    stripe_public_key: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+  },
 };
-const withVideos = require('next-videos')
+const withVideos = require("next-videos");
 module.exports = nextConfig;

e22ba8dc9f06346fe4ca0169e4808b07abdad8ab
slacker1992 <suvinjavax@gmail.com>
2023-03-17 09:24:10 +0530

    Update fetchAbout.ts

--- utils/fetchAbout.ts
+++ utils/fetchAbout.ts
@@ -1,7 +1,7 @@
 import { AboutType } from "./type";
 
 export const fetchAbout = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getAbout`);
+  const res = await fetch(`https://prestige-auto-care.vercel.app/api/getAbout`);
 
   const data = await res.json();
   const about: AboutType[] = data.about;

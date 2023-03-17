90def8b2513b95383319f85abea5c222d769fd2a
Tharinda Dissanayake <tharindadissanayake456@gmail.com>
2023-03-17 08:44:39 +0530

    about

--- pages/About.tsx
+++ pages/About.tsx
@@ -43,6 +43,8 @@ export default function About({ about, banner }: Props) {
   const handleCloseVideoPlayer = () => {
     setShowVideoPlayer(false);
   };
+
+  
   return (
     <Layout>
       
@@ -140,8 +142,99 @@ export default function About({ about, banner }: Props) {
         data-margin-bottom="225px"
       ></section>
       <div className="circle-bg space-bottom bg-smoke" />
-      <WhyChooseUs about={about} />
-      <GetPremiumParts banner={banner}/>
+      {/* <WhyChooseUs about={about} /> */}
+      <div className="bg-title position-relative overflow-hidden">
+      <div className="row">
+        <div className="col-xl-6">
+          <div className="as-video style1">
+            {showVideoPlayer && (
+              <div
+                className={`${stylesIndex.videoPlayerBG} ${
+                  showVideoPlayer ? stylesIndex.videPlayerShow : ""
+                }`}
+              >
+                <div className={stylesIndex.CloseIcon}>
+                  <AiOutlineClose onClick={handleCloseVideoPlayer} />
+                </div>
+                <div className={stylesIndex.videoPlayer}>
+                  <iframe
+                    width="100%"
+                    height="100%"
+                    src="https://www.youtube.com/embed/vlDOjTaaEdA"
+                    title="YouTube video player"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowFullScreen
+                    frameBorder="0"
+                  ></iframe>
+                </div>
+              </div>
+            )}
+            <img src={urlFor(about[0].choose_image).url()} alt="Video Image" />
+            <div
+              className="play-btn popup-video"
+              onClick={handleShowVideoPlayer}
+            >
+              <i className="fas fa-play" />
+            </div>
+          </div>
+        </div>
+        <div className="col-xl-6">
+          <div className="feature-media-wrap space">
+            <div className="title-area text-md-start text-center">
+              <span className="sub-title">About Our Company</span>
+              <h2 className="sec-title text-white">Why Choose Us?</h2>
+            </div>
+
+            {about.map((data: any, index: any) =>
+              data.whyChooseUs.map((item: any, key: any) => (
+                <div key={key} className="feature-media">
+                  <div className="feature-media_num">{Number(key) + 1}</div>
+                  <div className="feature-media_content">
+                    <h3 className="feature-media_title">{item.mainTitle}</h3>
+                    <p className="feature-media_text">{item.description}</p>
+                  </div>
+                </div>
+              ))
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="body-shape4">
+        <img src="assets/img/shape/shape_2.png" alt="shape" />
+      </div>
+    </div>
+      {/* <GetPremiumParts banner={banner}/> */}
+
+      <section
+      className="space"
+      data-overlay="title"
+      data-opacity={7}
+      // data-bg-src="assets/img/bg/cta_bg_1.jpg"
+      style={{ backgroundImage: `url('assets/img/bg/cta_bg_1.jpg')` }}
+    >
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-xl-7 col-lg-6 mb-5 mb-lg-0">
+            <div className="title-area mb-0 text-lg-start text-center">
+              <span className="sub-title text-white">Get Our Service</span>
+              <h2 className="sec-title text-white">
+                {banner[0]?.home_title}
+              </h2>
+            </div>
+          </div>
+          <div className="col-xl-5 col-lg-6">
+            <div className="btn-group justify-content-lg-end justify-content-center">
+              <Link href="service.html" className="as-btn style3">
+                Get Our Service
+              </Link>{" "}
+              <Link href="contact.html" className="as-btn style2">
+                Contact Us
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
     </Layout>
   );
 }

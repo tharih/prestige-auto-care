3882eb72d1ed1ccdd1af334746f7f6e3e6c99fd9
Tharinda Dissanayake <tharindadissanayake456@gmail.com>
2023-03-17 07:50:19 +0530

    sanity

---
+++ components/sanityComponents/Logo.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import React from "react";
+
+const Logo = (props: any) => {
+  const { renderDefault, title } = props;
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <Image
+        src="/assets/img/logo-white1.png"
+        alt="Prestige Auto Care Logo"
+        height={50}
+        width={50}
+        style={{
+          borderRadius: "50%",
+          objectFit: "cover",
+        }}
+      />
+      <>{renderDefault(props)}</>
+    </div>
+  );
+};
+
+export default Logo;
